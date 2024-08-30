efec731768d290cc18e858e71bbfb4b079def967
DesiZafirova <desszafirowa@gmail.com>
2024-08-30 15:14:25 +0300

    brandlist commit

--- src/components/BrandList.jsx
+++ src/components/BrandList.jsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components';
+
+const StyledBrandList = styled.section`
+  background-color: var(--color-grey-200);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 2rem 0;
+`;
+
+const Img = styled.img`
+  height: 6rem;
+  width: 6rem;
+`;
+
 function BrandList() {
-  return <div></div>;
+  return (
+    <StyledBrandList>
+      <Img src="../../public/brandlist/adidas.svg" />
+      <Img src="../../public/brandlist/nike.svg" />
+      <Img src="../../public/brandlist/converse.png" />
+      <Img src="../../public/brandlist/vans.png" />
+    </StyledBrandList>
+  );
 }
 
 export default BrandList;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import BrandList from '../components/BrandList';
 import Hero from '../components/Hero';
 
 function Homepage() {
   return (
     <>
       <Hero />
+      <BrandList />
     </>
   );
 }
