a283a10aaf51f5445c1b2accd63fd0b0fbbfc26f
DesiZafirova <desszafirowa@gmail.com>
2024-08-30 13:05:46 +0300

    creating page layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './components/AppLayout';
+import Homepage from './pages/Homepage';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="homepage" />} />
+            <Route path="homepage" element={<Homepage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,9 +1,31 @@
+import styled from 'styled-components';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+const StyledLayout = styled.div`
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 144rem;
+  margin: 0 auto;
+`;
+
 function AppLayout() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <StyledLayout>
+      <Header />
+      <Main>
+        <Container>
+          <Outlet />
+        </Container>
+      </Main>
+    </StyledLayout>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
