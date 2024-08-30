2c4f2f257865fd313eeacc1d899334252b7bbcf7
DesiZafirova <desszafirowa@gmail.com>
2024-08-30 17:05:59 +0300

    heading commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import Cart from './pages/Cart';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -50,7 +61,7 @@ function App() {
           },
         }}
       />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import Heading from './Heading';
 
 const StyledHero = styled.div`
   position: relative;
-  height: 100vh;
+  height: 90vh;
   overflow: hidden;
 `;
 
@@ -16,12 +17,43 @@ const Video = styled.video`
   z-index: -1;
 `;
 
+const HeroContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 65%;
+  transform: translateY(-50%);
+  background: rgba(255, 255, 255, 0.15);
+  border-radius: 8px;
+  padding: 2.4rem;
+  height: 50%;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeroText = styled.p`
+  font-size: 1.8rem;
+  color: var(--color-grey-500);
+`;
+
 function Hero() {
   return (
     <StyledHero>
       <Video autoPlay muted loop>
         <source src="../../public/hero.mp4" type="video/mp4" />
       </Video>
+      <HeroContent>
+        <div>
+          <Heading>
+            Step into style, <br /> walk with confidence
+          </Heading>
+          <HeroText>
+            &mdash; Discover the freshest sneaker drops and <br />
+            exclusive styles. Your ultimate destination for the latest in
+            sneaker culture.
+          </HeroText>
+        </div>
+      </HeroContent>
     </StyledHero>
   );
 }
