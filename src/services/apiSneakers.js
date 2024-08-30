aad164cd3310a3a386bcda7a10fe602073e47bee
DesiZafirova <desszafirowa@gmail.com>
2024-08-30 17:45:30 +0300

    getWomenSneakers and getMenSneakers

--- src/services/apiSneakers.js
+++ src/services/apiSneakers.js
@@ -9,3 +9,29 @@ export async function getSneakers() {
 
   return sneakers;
 }
+
+export async function getWomenSneakers() {
+  let { data: womenSneakers, error } = await supabase
+    .from('sneakers')
+    .select('*')
+    .eq('category', 'women');
+
+  if (error) {
+    throw new Error('Sneakers could not be loaded 💥');
+  }
+
+  return womenSneakers;
+}
+
+export async function getMenSneakers() {
+  let { data: menSneakers, error } = await supabase
+    .from('sneakers')
+    .select('*')
+    .eq('category', 'men');
+
+  if (error) {
+    throw new Error('Sneakers could not be loaded 💥');
+  }
+
+  return menSneakers;
+}
