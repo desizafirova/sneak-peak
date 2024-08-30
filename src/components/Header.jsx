41aa827457983555e5114f4724d4e60464508670
DesiZafirova <desszafirowa@gmail.com>
2024-08-30 12:53:11 +0300

    creating GlobalStyles and importing font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
 
 function Header() {
-  return <div></div>;
+  return <StyledHeader>Header</StyledHeader>;
 }
 
 export default Header;
