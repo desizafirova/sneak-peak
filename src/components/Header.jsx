cbfe653eac5796cbdb86999cc1c0eea942dbe12b
DesiZafirova <desszafirowa@gmail.com>
2024-08-31 12:45:31 +0300

    implementing fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 
 import MainNav from './MainNav';
 
@@ -6,13 +7,29 @@ const StyledHeader = styled.header`
   background-color: var(--color-grey-100);
   padding: 0 1.2rem;
   border-bottom: 1px solid var(--color-grey-100);
-  position: sticky;
-  top: 95;
+  position: ${(props) => (props.isFixed ? 'fixed' : 'static')};
+  top: 0;
+  width: 100%;
+  z-index: 1000;
 `;
 
 function Header() {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      const viewPortHeight = window.innerHeight;
+
+      setIsFixed(scrollPosition > viewPortHeight);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <StyledHeader>
+    <StyledHeader isFixed={isFixed}>
       <MainNav />
     </StyledHeader>
   );
