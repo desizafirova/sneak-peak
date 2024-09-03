21dcad05ea486e7f7d264669078a9bb51e28d7bf
DesiZafirova <desszafirowa@gmail.com>
2024-09-03 23:35:47 +0300

    modal functionality commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,10 @@
+import { cloneElement, createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
+import useOutsideClick from '../hooks/useOutsideClick';
+import { HiXMark } from 'react-icons/hi2';
+import { createPortal } from 'react-dom';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -18,4 +24,90 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100vh;
+  background-color: var(--backdrop-color);
+  backdrop-filter: blur(4px);
+  z-index: 1000;
+  transition: all 0.5s;
 `;
+
+const Button = styled.button`
+  background-color: none;
+  border: none;
+  padding: 0.4rem;
+  border-radius: var(--border-radius-sm);
+  transform: translateX(0.8rem);
+  transition: all 0.2s;
+  position: absolute;
+  top: 1.2rem;
+  right: 1.9rem;
+
+  &:hover {
+    background-color: var(--color-grey-50);
+  }
+
+  & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: var(--color-grey-500);
+  }
+`;
+
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = (name) => setOpenName(name);
+
+  return (
+    <ModalContext.Provider value={{ children, open, close, openName }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
+  return createPortal(
+    <Overlay>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
+          <HiXMark />
+        </Button>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+Open.propTypes = {
+  children: PropTypes.node.isRequired,
+  opens: PropTypes.string.isRequired,
+};
+
+Window.propTypes = {
+  children: PropTypes.node.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
